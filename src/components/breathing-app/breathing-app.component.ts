9fedecb4fdc350d706387d3c3817a312eae29bbd
Nathan <ncpenn@gmail.com>
2025-04-06 16:00:25 -0500

    using mat group toggle

--- src/components/breathing-app/breathing-app.component.ts
+++ src/components/breathing-app/breathing-app.component.ts
@@ -11,6 +11,7 @@ import { Subscription } from "rxjs";
 import { MatSelectModule } from "@angular/material/select";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatOptionModule } from "@angular/material/core";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
 
 @Component({
   selector: "breathing-app",
@@ -25,6 +26,7 @@ import { MatOptionModule } from "@angular/material/core";
     MatSelectModule,
     MatFormFieldModule,
     MatOptionModule,
+    MatButtonToggleModule,
   ],
   templateUrl: "./breathing-app.component.html",
   styleUrls: ["./breathing-app.component.scss"],
